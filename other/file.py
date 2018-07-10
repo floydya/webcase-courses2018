6b054d1014eeea49085413cc559ccfa7f6d30ad0
Vlad Galatskiy <xfloydya@gmail.com>
2018-07-10 16:36:04 +0300

    file.py 2 variant

--- other/file.py
+++ other/file.py
@@ -1,2 +1,6 @@
+# 1 variant
 with open("1.txt", "r") as f, open("g.txt", "w") as g: g.write(" ".join([x for x in f.read().replace("\n", " ").split(" ") if len(x) != 1 or len(x) != 2]))
 
+# 2 variant
+import re
+with open("1.txt", "r") as f, open("g.txt", "w") as g: g.write(" ".join(re.findall(r"\w{3,}", f.read())))
