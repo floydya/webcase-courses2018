6dfff040c5cda9a33680535ffe613221100cc5c0
xfloydya <xfloydya@gmail.com>
2018-07-17 09:25:51 +0300

    13

---
+++ homework/13/books/models.py
@@ -0,0 +1,66 @@
+from django.db import models
+from django.urls import reverse
+
+class Genre(models.Model):
+    name = models.CharField(max_length=200, help_text="Enter a book genre (e.g. Science Fiction, French Poetry etc.)")
+
+    def __str__(self):
+        return self.name
+
+
+class Language(models.Model):
+    name = models.CharField(max_length=100)
+
+    def __str__(self):
+        return self.name
+
+
+class Author(models.Model):
+    first_name = models.CharField(max_length=100)
+    last_name = models.CharField(max_length=100)
+    date_of_birth = models.DateField(null=True, blank=True)
+    date_of_death = models.DateField('Died', null=True, blank=True)
+
+    def get_absolute_url(self):
+        return reverse('author-detail', args=[str(self.pk)])
+
+    def __str__(self):
+        return '{0.last_name}, {0.first_name}'.format(self)
+
+
+class Book(models.Model):
+    title = models.CharField(max_length=200)
+    author = models.ForeignKey(Author, on_delete=models.SET_NULL, null=True)
+    summary = models.TextField(max_length=1000, help_text="Enter a brief description of the book")
+    isbn = models.CharField('ISBN', max_length=13, help_text="13 Character ISBN number")
+    genre = models.ManyToManyField(Genre, help_text="Select a genre for this book")
+
+    def __str__(self):
+        return self.title
+
+    def get_absolute_url(self):
+        return reverse('book-detail', args=[str(self.pk)])
+
+
+class BookInstance(models.Model):
+    book = models.ForeignKey(Book, on_delete=models.SET_NULL, null=True)
+    imprint = models.CharField(max_length=200)
+    due_back = models.DateField(null=True, blank=True)
+
+    language = models.ForeignKey(Language, on_delete=models.SET_NULL, null=True)
+
+    LOAN_STATUS = (
+        ('m', 'Maintenance'),
+        ('o', 'On loan'),
+        ('a', 'Available'),
+        ('r', 'Reserved'),
+    )
+
+    status = models.CharField(max_length=1, choices=LOAN_STATUS, blank=True, default='d', help_text='Book availability')
+
+    class Meta:
+        ordering = ['due_back']
+
+    def __str__(self):
+        return '{0.id} {0.title}'.format(self)
+
